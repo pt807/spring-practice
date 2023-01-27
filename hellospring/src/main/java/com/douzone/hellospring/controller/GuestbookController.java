a20fb260b0a7585041907c396604ab64f7edf965
pt807 <pt807@naver.com>
2023-01-27 17:08:37 +0900

    hello spring #4

--- hellospring/src/main/java/com/douzone/hellospring/controller/GuestbookController.java
+++ hellospring/src/main/java/com/douzone/hellospring/controller/GuestbookController.java
@@ -1,6 +1,5 @@
 package com.douzone.hellospring.controller;
 
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
