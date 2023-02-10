6af1c084069695ba912d56c78da768c99db9ab1e
pt807 <pt807@naver.com>
2023-02-10 11:30:41 +0900

    container #2

--- container/src/main/java/com/douzone/container/user/test/XmlConfigTest.java
+++ container/src/main/java/com/douzone/container/user/test/XmlConfigTest.java
@@ -2,21 +2,97 @@ package com.douzone.container.user.test;
 
 import org.springframework.beans.factory.BeanFactory;
 import org.springframework.beans.factory.xml.XmlBeanFactory;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 import org.springframework.core.io.ClassPathResource;
 
 import com.douzone.container.user.User;
 
 public class XmlConfigTest {
 	public static void main(String[] args) {
-		// XML Auto Configuration(Annotaion Test)
-		testBeanFactory01();
+		// XML Auto Configuration(Annotaion Scanning)
+		// testBeanFactory01();
+
+		// XML Auto Configuration(Explicit Configuration)
+		// testBeanFactory02();
+
+		// XML Auto Configuration(Annotaion Scanning)
+		 //testApplicationContext01();
+		
+		// XML Bean Configuration(Explicit Configuration)
+	    testApplicationContext02();
+	}
+
+	private static void testApplicationContext02() {
+		ApplicationContext ac = new ClassPathXmlApplicationContext("com/douzone/container/config/user/addlicationContext02.xml");
+		
+		User user = null;
+		
+		// id로 빈 가져오기
+		user = (User)ac.getBean("user");
+		System.out.println(user.getName());
+		
+		// name으로 빈 가져오기
+		user = (User)ac.getBean("usr");
+		System.out.println(user.getName());
+		
+		// Type으로 빈 가져오기
+		// 같은 타입의 빈이 2개 이상 있으면 Type으로 가져오기 실패
+		// id + type
+		// name + type
+		user = ac.getBean("user2", User.class);
+		System.out.println(user);
+		
+		// 파라미터 2개인 생성자로 생성된 빈 가져오기
+		user = ac.getBean("user3", User.class);
+		System.out.println(user);
+		
+		// 파라미터 2개인 생성자로 생성된 빈 가져오기
+		user = ac.getBean("user4", User.class);
+		System.out.println(user);
+		
+		// setter를 사용한 빈 가져오기1
+		user = ac.getBean("user5", User.class);
+		System.out.println(user);
+		
+		// setter를 사용한 빈 가져오기2 (DI)
+		user = ac.getBean("user6", User.class);
+		System.out.println(user);
+		
+		// setter를 사용한 빈 가져오기3 (Collection Property)
+		user = ac.getBean("user7", User.class);
+		System.out.println(user);
+
+	}
+
+	private static void testApplicationContext01() {
+		ApplicationContext ac = new ClassPathXmlApplicationContext("com/douzone/container/config/user/addlicationContext01.xml");
+		
+		User user = null;
+		
+		user = ac.getBean(User.class);
+		System.out.println(user.getName());
+		
+		// Annotation Scan 설정에서는 Bean id가 자동으로 부여된다.
+		user = (User)ac.getBean("user");
+		System.out.println(user.getName());
+
 	}
 
 	private static void testBeanFactory01() {
-		BeanFactory bf = 
-				new XmlBeanFactory(new ClassPathResource("com/douzone/container/config/user/addlicationContext.xml"));
-		
+		BeanFactory bf = new XmlBeanFactory(
+				new ClassPathResource("com/douzone/container/config/user/addlicationContext01.xml"));
+
 		User user = bf.getBean(User.class);
 		System.out.println(user.getName());
 	}
+
+	private static void testBeanFactory02() {
+		BeanFactory bf = new XmlBeanFactory(
+				new ClassPathResource("com/douzone/container/config/user/addlicationContext02.xml"));
+
+		User user = bf.getBean(User.class);
+		System.out.println(user.getName());
+	}
+
 }
