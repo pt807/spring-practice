aab2ee99a0cabad5d68bdbc4921f40ec18b865ba
pt807 <pt807@naver.com>
2023-02-13 13:03:07 +0900

    xml - exlicit configuration #finish

---
+++ container/src/main/java/com/douzone/container/videosystem/BlankDisc.java
@@ -0,0 +1,59 @@
+package com.douzone.container.videosystem;
+
+import java.util.List;
+
+public class BlankDisc implements DigitalVideoDisc {
+	private String title;
+	private String studio;
+	private List<String> actors;
+
+	public BlankDisc() {
+
+	}
+
+	public BlankDisc(String title, String studio) {
+		this.title = title;
+		this.studio = studio;
+	}
+
+	public BlankDisc(String title, String studio, List<String> actors) {
+		this.title = title;
+		this.studio = studio;
+		this.actors = actors;
+	}
+
+	@Override
+	public String play() {
+		return "Playing Movie " + studio + "'s " + title;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public String getStudio() {
+		return studio;
+	}
+
+	public void setStudio(String studio) {
+		this.studio = studio;
+	}
+
+	public List<String> getActors() {
+		return actors;
+	}
+
+	public void setActors(List<String> actors) {
+		this.actors = actors;
+	}
+
+	@Override
+	public String toString() {
+		return "BlankDisc [title=" + title + ", studio=" + studio + ", actors=" + actors + "]";
+	}
+
+}
